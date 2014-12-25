fcbfbb0f9e8e78f33103769ef8d57f9ed99ae962
Deven Bhooshan <devenbhooshan@gmail.com>
2014-12-25 22:40:09 +0530

    Front page HTML now generated from a template

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-	return render(request,'home.html')
-	
+    return render(request,'home.html')
+    
 
 
