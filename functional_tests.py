0d343c4c925ff6557cbe4b8a72bacb28083cce4e
Deven Bhooshan <devenbhooshan@gmail.com>
2014-12-26 00:01:17 +0530

    SC

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self): 
         self.browser.quit()
+
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Guru opens the homepage of To Do app
         self.browser.get("http://localhost:8000")
@@ -27,13 +34,10 @@ class NewVisitorTest(unittest.TestCase):
         # Types "Find love" in the inputbox
         inputbox.send_keys("Find Love")
         inputbox.send_keys(Keys.ENTER)
-        
-        
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Find Love', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly' ,[row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
